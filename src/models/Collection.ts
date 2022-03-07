3ed5d2cf25d12c17867ae56a0955bdb119008661
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-03-07 12:11:55 -0800

    generic User Collection

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -3,11 +3,14 @@ import { Eventing } from "./Eventing";
 import { User, UserProps } from "./User";
 
 
-export class Collection {
-  models: User[] = [];
+export class Collection<T, K> {
+  models: T[] = [];
   events: Eventing = new Eventing();
 
-  constructor(public rootUrl: string) {}
+  constructor(
+    public rootUrl: string,
+    public deserialize: (json: K) => T // take in some json data and turn it into an instance of an object.
+    ) {}
 
   get on() {
     return this.events.on;
@@ -20,9 +23,8 @@ export class Collection {
   fetch(): void {
     axios.get(this.rootUrl)
       .then((response: AxiosResponse) => {
-        response.data.forEach((value: UserProps) => {
-          const user = User.buildUser(value);
-          this.models.push(user)
+        response.data.forEach((value: K) => {
+          this.models.push(this.deserialize(value));
         });
 
         this.trigger('change');
--- web-framework-app.md
+++ web-framework-app.md
@@ -1129,3 +1129,43 @@ axios.get('http://localhost:3000/users')
 // {name: 'new record', age: 0, id: 2}
 
 ```
+
+## Making class Collection Generic\
+Currently have:
+```typescript
+import axios, { AxiosResponse } from "axios";
+import { Eventing } from "./Eventing";
+import { User, UserProps } from "./User";
+
+
+export class Collection {
+  models: User[] = [];
+  events: Eventing = new Eventing();
+
+  constructor(public rootUrl: string) {}
+
+  get on() {
+    return this.events.on;
+  }
+  
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  fetch(): void {
+    axios.get(this.rootUrl)
+      .then((response: AxiosResponse) => {
+        response.data.forEach((value: UserProps) => {
+          const user = User.buildUser(value);
+          this.models.push(user)
+        });
+
+        this.trigger('change');
+      });
+
+  }
+}
+
+```
+
+K will specify the type of data we get back.
